b9720b75c29ee14d564191ed021c2b72801a2725
Latouring204 <ojedaleandro911@gmail.com>
2025-07-06 11:48:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ CAMPOS = [
     "Nombre y apellido", "Legajo personal", "DNI", "Domicilio", "Teléfono", "Número de obra social",
     "Situación de revista", "Marca de armamento", "Número de serie del arma", "Número de armamento",
     "Marca de chaleco balístico", "Modelo del chaleco", "Número de serie del chaleco",
-    "Número de chaleco", "Fecha de fabricación del chaleco",
+    "Número de lote de chaleco", "Fecha de fabricación del chaleco",
     "¿Posee bastón tonfa asignado?", "¿Piloto de lluvia policial asignado?"
 ]
 
@@ -50,3 +50,38 @@ if st.button("Guardar Datos"):
 
 if st.checkbox("Mostrar todos los registros"):
     st.dataframe(df)
+    import streamlit as st
+import pandas as pd
+
+# Cargar datos
+df = pd.read_csv('empleados.csv')
+
+# CSS para cambiar fondo
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e");
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title('Base de Datos de Empleados')
+
+# Buscador por DNI
+dni = st.text_input('Buscar por DNI:')
+
+if dni:
+    resultado = df[df['DNI'] == int(dni)]
+    st.write(resultado)
+else:
+    st.write(df)
+    <style>
+.stApp {
+    background-image: "url(https://www.google.com/search?q=imagen+de+pfa&oq=imagen+de+pfa&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yCggCEAAYgAQYogQyCggDEAAYgAQYogQyBwgEEAAY7wUyBwgFEAAY7wXSAQg2Mzg5ajBqN6gCFLACAfEF3dElwStEwzA&client=ms-android-samsung-ss&sourceid=chrome-mobile&ie=UTF-8#vhid=sH-et-wctkp1nM&vssid=_J4xqaIWyJZOf5OUP4qGemQI_50");
+    background-size: cover;
+}
+</style>
